a6aa9d2813f9c88e767827589d9d1c5780baad64
Jawid Jahish <jawidjahish@Jawids-MBP.attlocal.net>
2022-06-22 13:47:00 -0700

    third practice

---
+++ src/gitthirdpractice/ThirdPractice.java
@@ -0,0 +1,17 @@
+package gitthirdpractice;
+
+public class ThirdPractice {
+
+	public static void main(String[] args) {
+
+    int a = 11;
+    int b = 12;
+    if( a > b) {
+    	System.out.println(a + " is greater than " + b);	
+    } else  { 
+    
+	} 
+		System.out.println(b+ " is greater than " + a);
+	}
+
+}
